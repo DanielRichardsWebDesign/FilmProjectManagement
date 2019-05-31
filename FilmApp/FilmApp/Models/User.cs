19ebe74a342438fb24ad7b5fd5af12a7dffd5df9
Daniel Richards <40298831+DanielRichardsWebDesign@users.noreply.github.com>
2019-06-01 00:39:50 +0100

    Added User class

---
+++ FilmApp/FilmApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmApp.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public bool UserEmailApproved { get; set; }
+        public string UserAccountName { get; set; }
+        public string UserPass { get; set; }
+    }
+}
